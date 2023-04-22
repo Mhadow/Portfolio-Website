51da1ce3ba20433371c0fe2ae23ec10c1b6d7466
Mhadow <mad_mada2012@yahoo.com>
2023-04-22 11:56:09 +0300

    Added content and improved responsivness

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// vars
+// VARS
 
 const btnScroll = document.getElementById("scrollTop");
 const projEls = Array.from(document.getElementsByClassName("card-proj"));
@@ -14,6 +14,24 @@ const phoneEl = document.getElementById("contact-phone");
 const toastEl = document.getElementById("toast-el");
 const toastEl2 = document.getElementById("toast-el2");
 const contactSectionEl = document.getElementById("contact");
+const aboutSectionEl = document.getElementById("info");
+
+// tech items elements
+
+const techItem1 = document.getElementById("tech1");
+const techItem2 = document.getElementById("tech2");
+const techItem3 = document.getElementById("tech3");
+const techItem4 = document.getElementById("tech4");
+const techItem5 = document.getElementById("tech5");
+const techItem6 = document.getElementById("tech6");
+const techItem7 = document.getElementById("tech7");
+
+const techSection = document.getElementById("technologies");
+
+const techHeader1 = document.getElementById("tech-header1");
+const techHeader2 = document.getElementById("tech-header2");
+
+// FUNCTIONS
 
 // tooltip initialization
 
@@ -30,21 +48,33 @@ function scrollToTop() {
   document.documentElement.scrollTop = 0;
 }
 
-// shows panel with extra info for each project
+// sHOWS PANEL WITH EXTRA INFO FOR EACH PROJECT
 
-for (let i = 0; i < projEls.length; i++) {
-  projEls[i].addEventListener("mouseover", () => {
-    panelEls[i].style.top = 0;
-  });
-}
+// for (let i = 0; i < projEls.length; i++) {
+//   projEls[i].addEventListener("mouseover", () => {
+//     panelEls[i].style.top = 0;
+//   });
+// }
 
-// hides panel with extra info for each project
+// only for 1, for now
 
-for (let j = 0; j < projEls.length; j++) {
-  projEls[j].addEventListener("mouseleave", () => {
-    panelEls[j].style.top = "-1000px";
-  });
-}
+projEls[0].addEventListener("mouseover", () => {
+  panelEls[0].style.top = 0;
+});
+
+// HIDES PANEL WITH EXTRA INFO FOR EACH PROJECT
+
+// for (let j = 0; j < projEls.length; j++) {
+//   projEls[j].addEventListener("mouseleave", () => {
+//     panelEls[j].style.top = "-1000px";
+//   });
+// }
+
+// only for 1, for now
+
+projEls[0].addEventListener("mouseleave", () => {
+  panelEls[0].style.top = "-1000px";
+});
 
 // scroll observer hint
 
@@ -99,9 +129,9 @@ f2El.addEventListener("dblclick", hideBox2);
 
 const observerInfo1 = new IntersectionObserver((els) => {
   els.forEach((el) => {
-    if (!el.isIntersecting && window.innerWidth > 986) {
+    if (!el.isIntersecting && window.innerWidth > 768) {
       el.target.style.visibility = "hidden";
-    } else if (window.innerWidth < 986) {
+    } else if (window.innerWidth < 768) {
       el.target.style.visibility = "visible";
     }
   });
@@ -111,9 +141,9 @@ observerInfo1.observe(document.querySelector(".card-ed"));
 
 const observerInfo2 = new IntersectionObserver((els) => {
   els.forEach((el) => {
-    if (!el.isIntersecting && window.innerWidth > 986) {
+    if (!el.isIntersecting && window.innerWidth > 768) {
       el.target.style.visibility = "hidden";
-    } else if (window.innerWidth < 986) {
+    } else if (window.innerWidth < 768) {
       el.target.style.visibility = "visible";
     }
   });
@@ -124,19 +154,19 @@ observerInfo2.observe(document.querySelector(".card-ed2"));
 // hides/shows boxes of info (from the timeline section) that were left visible/hidden, depending on screen size
 
 window.addEventListener("resize", () => {
-  if (window.innerWidth < 968 && infoBoxEl1.style.visibility === "hidden") {
+  if (window.innerWidth < 768 && infoBoxEl1.style.visibility === "hidden") {
     showBox1();
   } else if (
-    window.innerWidth > 968 &&
+    window.innerWidth > 768 &&
     infoBoxEl1.style.visibility === "visible"
   ) {
     hideBox1();
   }
 
-  if (window.innerWidth < 968 && infoBoxEl2.style.visibility === "hidden") {
+  if (window.innerWidth < 768 && infoBoxEl2.style.visibility === "hidden") {
     showBox2();
   } else if (
-    window.innerWidth > 968 &&
+    window.innerWidth > 768 &&
     infoBoxEl2.style.visibility === "visible"
   ) {
     hideBox2();
@@ -164,7 +194,7 @@ listEls[1].addEventListener("mouseleave", () => {
 // hover behaviour for box 3 of info in contact section
 
 listEls[2].addEventListener("mouseover", () => {
-  listEls[2].textContent = "mail@mail.com";
+  listEls[2].textContent = "madalinrobu18@gmail.com";
 });
 listEls[2].addEventListener("mouseleave", () => {
   listEls[2].textContent = "hover over me ";
@@ -173,7 +203,7 @@ listEls[2].addEventListener("mouseleave", () => {
 // hover behaviour for box 4 of info in contact section
 
 listEls[3].addEventListener("mouseover", () => {
-  listEls[3].textContent = "928392083";
+  listEls[3].textContent = "0767894078";
 });
 listEls[3].addEventListener("mouseleave", () => {
   listEls[3].textContent = "hover over me ";
@@ -211,3 +241,47 @@ const boxesObserver = new IntersectionObserver((els) => {
 });
 
 boxesObserver.observe(contactSectionEl);
+
+// animates tech items on scroll
+
+const techObserver = new IntersectionObserver((els) => {
+  els.forEach((el) => {
+    if (el.isIntersecting) {
+      techHeader1.classList.add("header-anim");
+      techHeader2.classList.add("header-anim");
+      techItem1.classList.add("tech-item1-anim");
+      techItem2.classList.add("tech-item2-anim");
+      techItem3.classList.add("tech-item3-anim");
+      techItem4.classList.add("tech-item4-anim");
+      techItem5.classList.add("tech-item5-anim");
+      techItem6.classList.add("tech-item6-anim");
+      techItem7.classList.add("tech-item7-anim");
+    } else {
+      techHeader1.classList.remove("header-anim");
+      techHeader2.classList.remove("header-anim");
+      techItem1.classList.remove("tech-item1-anim");
+      techItem2.classList.remove("tech-item2-anim");
+      techItem3.classList.remove("tech-item3-anim");
+      techItem4.classList.remove("tech-item4-anim");
+      techItem5.classList.remove("tech-item5-anim");
+      techItem6.classList.remove("tech-item6-anim");
+      techItem7.classList.remove("tech-item7-anim");
+    }
+  });
+});
+
+techObserver.observe(techSection);
+
+// hides the "back to top" button when the page is at the top
+
+const aboutObserver = new IntersectionObserver((els) => {
+  els.forEach((el) => {
+    if (el.isIntersecting) {
+      btnScroll.classList.add("redirect-hide");
+    } else {
+      btnScroll.classList.remove("redirect-hide");
+    }
+  });
+});
+
+aboutObserver.observe(aboutSectionEl);
